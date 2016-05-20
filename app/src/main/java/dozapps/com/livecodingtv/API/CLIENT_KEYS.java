dce75b6b9ae1a0b46f652b0d9b0358838e65bed5
Vairon <vairon@live.com>
2016-05-20 03:59:31 -0400

    Update CLIENT_KEYS.java

--- app/src/main/java/dozapps/com/livecodingtv/API/CLIENT_KEYS.java
+++ app/src/main/java/dozapps/com/livecodingtv/API/CLIENT_KEYS.java
@@ -4,5 +4,5 @@ package dozapps.com.livecodingtv.API;
  * Created by vaironl on 5/15/16.
  */
 public class CLIENT_KEYS {
-    public static final String CLIENT_ID ="TvOTQbsts6jLmXt3Z0a5xKBilikjG6sx4UlKDmyB" ;
+    public static final String CLIENT_ID ="KEYHERE" ;
 }
